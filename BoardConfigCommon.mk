be16592d39219bd54541c80c9697e8fbb77d9a13
Talustus <talustus@dream-irc.de>
2015-03-29 19:49:56 +0200

    fix illegal instruction error

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -22,7 +22,7 @@ TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
-TARGET_CPU_VARIANT := cortex-a15
+TARGET_CPU_VARIANT := krait
 TARGET_ARCH_VARIANT := armv7-a-neon
 
 # Charger
